f73c7faf98451c3e75b247ac688263f80551f9d4
mjmorse <>
2014-04-17 15:44:46 +0000

    #bingshengswag

--- src/system/coordinator/multiserver/BingshengEncryptionScheme.java
+++ src/system/coordinator/multiserver/BingshengEncryptionScheme.java
@@ -42,39 +42,63 @@ public class BingshengEncryptionScheme {
 		
 		return null;
 	}
-	
+
 	public static ArrayList<BigInteger> reencrypt(ArrayList<BigInteger> messages, BigInteger key ){
 		Runtime rt = Runtime.getRuntime();
 		try {
-			String command = "/bingyzhang/ReEnc AIUiUE/kjQmLfJu514zVw/NzOlHc ";
+
 			int iter = 200;
-			Random rand = new Random();
-			for(int i = 0; i < iter; i ++){
-				BigInteger randomInt = new BigInteger(190, rand);				
-				command = command + " "+new String(Base64.encodeBase64(randomInt.toByteArray()));
+			ArrayList< String > toBingsheng = new ArrayList<String>();
+			int i = 0;
+			while( i < messages.size()){
+				String command = "/bingyzhang/ReEnc AIUiUE/kjQmLfJu514zVw/NzOlHc ";
+				int j = 0;
+				while(j < iter){
+					BigInteger message = messages.get(i+j);
+					command = command + " "+new String(Base64.encodeBase64(message.toByteArray()));
+					j++;
+				}
+				System.out.println(command);
+				toBingsheng.add(command);
+				i += j;
 			}
-			
-			Process p = rt.exec(System.getProperty("user.dir")+command);
-			p.waitFor(); 
+			ArrayList<Process> processes = new ArrayList<Process>();
 
-			InputStream is = p.getInputStream();
-			BufferedReader r = new BufferedReader(new InputStreamReader(is));
+			for(String command :toBingsheng){
+				processes.add(rt.exec(System.getProperty("user.dir")+command));
+			}
+			for(Process p : processes){
+				p.waitFor(); 
+			}
+			System.out.println("finsihed!");
 			ArrayList<BigInteger> encryptedBigInts = new ArrayList<BigInteger>();
-			
-			for(int i = 0; i < iter; i ++){
-				BigInteger returnedInt = new BigInteger(Base64.decodeBase64(r.readLine()));
-				encryptedBigInts.add(returnedInt);
-			}
-			return encryptedBigInts;
 
+			for(Process p : processes){
+				InputStream is = p.getInputStream();
+				BufferedReader r = new BufferedReader(new InputStreamReader(is));
+
+				for(int j = 0; j < iter; j ++){
+					String s= r.readLine();
+					System.out.println(s);
+					BigInteger returnedInt = new BigInteger(Base64.decodeBase64(s));
+					encryptedBigInts.add(returnedInt);
+				}
+				return encryptedBigInts;
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
-		return null;	}
+
+
+		return null;
+	}
 	
 	public static void main(String[] args){
-		reencrypt(null,null);
+		ArrayList<BigInteger> testInts = new ArrayList<BigInteger>();
+		Random r = new Random();
+		for(int i = 0; i < 5000; i++)
+			testInts.add(new BigInteger(190,r));	
+		System.out.println("made some ints");
+		reencrypt(testInts,null);
 	}
 }
